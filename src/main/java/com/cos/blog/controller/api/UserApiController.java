f5f465f4cb7d6a2adc93312b992706f49e8ecd7f
Minkon Kim <alsrhs98@gmail.com>
2023-09-15 10:38:27 +0900

    세션값 변경 구현 2023.09.15

--- src/main/java/com/cos/blog/controller/api/UserApiController.java
+++ src/main/java/com/cos/blog/controller/api/UserApiController.java
@@ -1,19 +1,23 @@
 package com.cos.blog.controller.api;
 
 
+import com.cos.blog.config.auth.PrincipalDetail;
 import com.cos.blog.dto.ResponseDto;
-import com.cos.blog.model.RoleType;
 import com.cos.blog.model.User;
 import com.cos.blog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpSession;
+import java.security.Principal;
 
 @RestController
 public class UserApiController {
@@ -34,9 +38,13 @@ public class UserApiController {
 
 
     @PutMapping("/user")
-    public ResponseDto<Integer> update(@RequestBody User user){
+    public ResponseDto<Integer> update(@RequestBody User user, @AuthenticationPrincipal PrincipalDetail principal){
         userService.회원수정(user);
         //DB값은 변경됬지만 session값은 변경되지 않았다.
+        //직접 세션값을 변경해 줄거임.
+        Authentication authentication = new UsernamePasswordAuthenticationToken(principal, null);
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+
         return new ResponseDto<Integer>(HttpStatus.OK.value(),1);
     }
 
